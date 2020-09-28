c9b4b8d9a7adc7d0bbb25b4828bbda3bce330afd
sinakhalili <khalili@sfu.ca>
2020-09-27 23:34:20 -0700

    Add new projects

--- web/src/containers/layout.js
+++ web/src/containers/layout.js
@@ -31,7 +31,7 @@ function LayoutContainer (props) {
           <Layout
             {...props}
             showNav={showNav}
-            siteTitle={data.site.title}
+            siteTitle="sina khalili portfolio home"
             onHideNav={handleHideNav}
             onShowNav={handleShowNav}
           />
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import {graphql} from 'gatsby'
+import React from "react";
+import { graphql } from "gatsby";
 import {
   mapEdgesToNodes,
   filterOutDocsWithoutSlugs,
   filterOutDocsPublishedInTheFuture
-} from '../lib/helpers'
-import Container from '../components/container'
-import GraphQLErrorList from '../components/graphql-error-list'
-import ProjectPreviewGrid from '../components/project-preview-grid'
-import SEO from '../components/seo'
-import Layout from '../containers/layout'
+} from "../lib/helpers";
+import Container from "../components/container";
+import GraphQLErrorList from "../components/graphql-error-list";
+import ProjectPreviewGrid from "../components/project-preview-grid";
+import SEO from "../components/seo";
+import Layout from "../containers/layout";
 
 export const query = graphql`
   query IndexPageQuery {
@@ -17,6 +17,7 @@ export const query = graphql`
       title
       description
       keywords
+      subtitle
     }
     projects: allSanitySampleProject(
       limit: 6
@@ -60,44 +61,45 @@ export const query = graphql`
 `
 
 const IndexPage = props => {
-  const {data, errors} = props
+  const { data, errors } = props;
 
   if (errors) {
     return (
       <Layout>
         <GraphQLErrorList errors={errors} />
       </Layout>
-    )
+    );
   }
 
-  const site = (data || {}).site
+  const site = (data || {}).site;
   const projectNodes = (data || {}).projects
     ? mapEdgesToNodes(data.projects)
-      .filter(filterOutDocsWithoutSlugs)
-      .filter(filterOutDocsPublishedInTheFuture)
-    : []
+        .filter(filterOutDocsWithoutSlugs)
+        .filter(filterOutDocsPublishedInTheFuture)
+    : [];
 
   if (!site) {
     throw new Error(
       'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
-    )
+    );
   }
 
   return (
     <Layout>
       <SEO title={site.title} description={site.description} keywords={site.keywords} />
       <Container>
-        <h1 hidden>Welcome to {site.title}</h1>
+        <h1>Hey Dapper Labs! welcome to {site.title} 👌</h1>
+        <h3>If you are not from Axiom Zen, just know that I view all employers equally (but some are more equal than others)</h3>
         {projectNodes && (
           <ProjectPreviewGrid
-            title='Latest projects'
+            title="Latest projects"
             nodes={projectNodes}
-            browseMoreHref='/archive/'
+            browseMoreHref="/archive/"
           />
         )}
       </Container>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
